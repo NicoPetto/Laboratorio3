86f55d3220d90f3921b14b692d8cc6b9832bba0f
Elizeth <elizethcedres@gmail.com>
2026-06-30 14:42:01 -0300

    Update Menu.h

--- Presentacion/Menu.h
+++ Presentacion/Menu.h
@@ -4,13 +4,14 @@
 
 #ifndef LAB_2_MENU_H
 #define LAB_2_MENU_H
-
-
+#include "../Negocio/ISistema.h"
 
 class Menu {
-
+ private:
+    ISistema* sistema;
+public:
+    Menu();
+    void menuPrincipal();
 };
-
-
 
 #endif //LAB_2_MENU_H
